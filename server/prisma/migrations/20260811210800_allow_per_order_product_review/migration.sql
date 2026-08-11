80ae4c1e3f3381e28e8200aab5f727a1d31bf370
ductnf8 <ductnf8@fullstack.edu.vn>
2026-08-11 23:09:27 +0700

    fix(prisma): update migration SQL script with IF NOT EXISTS guards

--- server/prisma/migrations/20260811210800_allow_per_order_product_review/migration.sql
+++ server/prisma/migrations/20260811210800_allow_per_order_product_review/migration.sql
@@ -1,14 +1,15 @@
 -- AlterTable
-ALTER TABLE "product_reviews" ADD COLUMN "orderId" TEXT;
+ALTER TABLE "product_reviews" ADD COLUMN IF NOT EXISTS "orderId" TEXT;
 
 -- DropIndex
 DROP INDEX IF EXISTS "product_reviews_userId_productId_key";
 
 -- CreateIndex
-CREATE UNIQUE INDEX "product_reviews_userId_productId_orderId_key" ON "product_reviews"("userId", "productId", "orderId");
+CREATE UNIQUE INDEX IF NOT EXISTS "product_reviews_userId_productId_orderId_key" ON "product_reviews"("userId", "productId", "orderId");
 
 -- CreateIndex
-CREATE INDEX "product_reviews_orderId_idx" ON "product_reviews"("orderId");
+CREATE INDEX IF NOT EXISTS "product_reviews_orderId_idx" ON "product_reviews"("orderId");
 
 -- AddForeignKey
+ALTER TABLE "product_reviews" DROP CONSTRAINT IF EXISTS "product_reviews_orderId_fkey";
 ALTER TABLE "product_reviews" ADD CONSTRAINT "product_reviews_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
