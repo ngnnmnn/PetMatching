0521888e50f10469c63ffb83384fc326fb042212
ductnf8 <ductnf8@fullstack.edu.vn>
2026-08-13 00:30:22 +0700

    fix: update migration script for wishlist table to use DROP TABLE IF EXISTS CASCADE

--- server/prisma/migrations/20260812235000_remove_wishlist_table/migration.sql
+++ server/prisma/migrations/20260812235000_remove_wishlist_table/migration.sql
@@ -1,6 +1,2 @@
--- DropForeignKey
-ALTER TABLE "public"."wishlist_items" DROP CONSTRAINT IF EXISTS "wishlist_items_productId_fkey";
-ALTER TABLE "public"."wishlist_items" DROP CONSTRAINT IF EXISTS "wishlist_items_userId_fkey";
-
 -- DropTable
-DROP TABLE IF EXISTS "public"."wishlist_items";
+DROP TABLE IF EXISTS "public"."wishlist_items" CASCADE;
